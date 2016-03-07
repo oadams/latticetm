4858cb59aa22b5ed023604858fb939e3c3f4405d
Oliver Adams <oliver.adams@gmail.com>
2016-03-07 11:13:59 +0900

    Debug file searching for pairs where the TM is better/worse than plain lattice.

--- src/scripts/debug.py
+++ src/scripts/debug.py
@@ -1,33 +1,56 @@
-#!/usr/bin/python3
+#!/usr/bin/python2
+from __future__ import print_function
 """
 This is a script for the purpose of more closely examining the transcriptions
 and alignments found with latticelm.
 """
 import sys
 import io
+import distance
 
 with io.open(sys.argv[1], encoding="utf8") as plain_f:
     plain_lines = plain_f.readlines()
-with io.open(sys.argv[2], encoding="utf8") as tm_one_best_f:
-    tm_one_best_lines = tm_one_best_f.readlines()
+with io.open(sys.argv[2], encoding="utf8") as tm_f:
+    tm_lines = tm_f.readlines()
+with io.open(sys.argv[3], encoding="utf8") as gold_f:
+    gold_lines = gold_f.readlines()
 #with io.open(sys.argv[3], encoding="utf8") as one_best_f:
 #    one_best_lines = one_best_f.readlines()
 
 plain_lines = [line.strip() for line in plain_lines]
 #one_best_lines = [line.strip() for line in one_best_lines]
-tm_one_best_lines = [line.strip() for line in tm_one_best_lines]
+tm_lines = [line.strip() for line in tm_lines]
+gold_lines = [line.strip() for line in gold_lines]
 
-assert len(plain_lines) == len(tm_one_best_lines)
-#assert len(plain_lines) == len(one_best_lines)
+assert len(plain_lines) == len(tm_lines)
+assert len(plain_lines) == len(gold_lines)
+
 i = 0
-count = 0 
+count_better = 0
+count_worse = 0
+count_same = 0
 for i in range(len(plain_lines)):
-    if plain_lines[i].strip() != tm_one_best_lines[i]:
-        print("Sentence %d is different" % i)
-        print("Plain:\t", plain_lines[i])
+    gold_line = gold_lines[i].split()
+    plain_line = plain_lines[i].split()
+    tm_line = tm_lines[i].split()
+    plain_dist = distance.min_edit_distance(gold_line, plain_line)
+    tm_dist = distance.min_edit_distance(gold_line, tm_line)
+
+    if tm_dist < plain_dist:
+        count_better += 1
+    elif tm_dist > plain_dist:
+        print(i)
+        print("Gold:\t", gold_line)
+        print("Plain (med %d):\t" % plain_dist, plain_line)
         #print("1best:\t", one_best_lines[i])
-        print("TM:\t", tm_one_best_lines[i])
+        print("TM (med %d):\t" % tm_dist, tm_line)
         print("===")
-        count += 1
-        input()
-print(count, "/", len(plain_lines))
+        raw_input()
+        count_worse += 1
+    else:
+        count_same += 1
+    i += 1
+
+print(count_better)
+print(count_worse)
+print(count_same)
