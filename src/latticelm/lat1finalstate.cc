21ef0ab192adec72c5b6cb9db5de7e80b77c8dd0
Oliver Adams <oliver.adams@gmail.com>
2017-05-16 13:11:57 +1000

    Added executable to make there be just one final state.

---
+++ src/latticelm/lat1finalstate.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fst/fstlib.h>
+
+using namespace fst;
+
+int main()
+{
+  VectorFst<LogArc> *model = VectorFst<LogArc>::Read("input.fst");
+
+  // Create a new final state.
+  LogArc::StateId final_state_id = model->AddState();
+  // Set One True Final state to Zero for now, will change to One later.
+  model->SetFinal(final_state_id, LogWeight::Zero());
+
+  // Change the weights for all current final states, and add free epsilon
+  // transitions to the One True Final state.
+  for (StateIterator<VectorFst<LogArc>> siter(*model); !siter.Done(); siter.Next()) {
+    LogArc::StateId state_id = siter.Value();
+
+    // If the state is final, then use the weight in an epsilon transition to
+    // the final state, and change the current state's weight to zero.
+    if (model->Final(state_id) != LogWeight::Zero()) {
+      LogWeight weight = model->Final(state_id);
+      // Add an arc to the new final state.
+      model->AddArc(state_id, LogArc(0, 0, weight, final_state_id));
+      // Make the state non-final.
+      model->SetFinal(state_id, LogWeight::Zero());
+    }
+  }
+
+  model->SetFinal(final_state_id, LogWeight::One());
+
+  model->Write("output.fst");
+}
